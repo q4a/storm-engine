82e529489f848e05a0007bbc719ae334d1bf2566
Aleksey Komarov <q4arus@ya.ru>
2021-12-17 21:49:06 +0300

    [math] replace D3DXQUATERNION with math3d/Quaternion.h

--- src/libs/animation/src/AnimationImp.cpp
+++ src/libs/animation/src/AnimationImp.cpp
@@ -239,12 +239,14 @@ void AnimationImp::BuildAnimationMatrices()
             for (long j = 0; j < nbones; j++)
             {
                 auto &bn = aniInfo->GetBone(j);
+                Matrix tmpMtx;
                 CMatrix inmtx;
-                D3DXQUATERNION qt0, qt1, qt;
+                Quaternion qt0, qt1, qt;
                 bn.BlendFrame(f0, ki0, qt0);
                 bn.BlendFrame(f1, ki1, qt1);
-                D3DXQuaternionSlerp(&qt, &qt0, &qt1, kBlend);
-                D3DXMatrixRotationQuaternion(inmtx, &qt);
+                qt.SLerp(qt0, qt1, kBlend);
+                qt.GetMatrix(tmpMtx);
+                inmtx = tmpMtx;
                 inmtx.Pos() = bn.pos0;
                 if (j == 0)
                 {
@@ -274,10 +276,12 @@ void AnimationImp::BuildAnimationMatrices()
             for (long j = 0; j < nbones; j++)
             {
                 auto &bn = aniInfo->GetBone(j);
+                Matrix tmpMtx;
                 CMatrix inmtx;
-                D3DXQUATERNION qt;
+                Quaternion qt;
                 bn.BlendFrame(f, ki, qt);
-                D3DXMatrixRotationQuaternion(inmtx, &qt);
+                qt.GetMatrix(tmpMtx);
+                inmtx = tmpMtx;
                 inmtx.Pos() = bn.pos0;
                 if (j == 0)
                     inmtx.Pos() = bn.pos[f] + ki * (bn.pos[f + 1] - bn.pos[f]);
@@ -303,10 +307,12 @@ void AnimationImp::BuildAnimationMatrices()
             for (long j = 0; j < nbones; j++)
             {
                 auto &bn = aniInfo->GetBone(j);
+                Matrix tmpMtx;
                 CMatrix inmtx;
-                D3DXQUATERNION qt;
+                Quaternion qt;
                 bn.BlendFrame(f, ki, qt);
-                D3DXMatrixRotationQuaternion(inmtx, &qt);
+                qt.GetMatrix(tmpMtx);
+                inmtx = tmpMtx;
                 inmtx.Pos() = bn.pos0;
                 if (j == 0)
                     inmtx.Pos() = bn.pos[f] + ki * (bn.pos[f + 1] - bn.pos[f]);
