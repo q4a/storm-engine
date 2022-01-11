8e3a31503b65c70670e591f97d6a5557e32dae12
Aleksey Komarov <q4arus@ya.ru>
2022-01-11 13:00:42 +0300

    [linux] add CI for Linux, dxvk_native, safeclib and 1st version for storm_platform.h

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class StormEngine(ConanFile):
 
     # dependencies used in deploy binaries
     # conan-center
-    requires = ["zlib/1.2.11", "spdlog/1.9.2", "sentry-native/0.4.13@storm/patched", "7zip/19.00", "fast_float/3.4.0", "sdl/2.0.18",
+    requires = ["zlib/1.2.11", "spdlog/1.9.2", "sentry-native/0.4.13@storm/patched", "fast_float/3.4.0", "sdl/2.0.18",
     # storm.jfrog.io
     "directx/9.0@storm/prebuilt", "fmod/2.02.05@storm/prebuilt"]
     # aux dependencies (e.g. for tests)
@@ -24,6 +24,12 @@ class StormEngine(ConanFile):
 
     # optional dependencies
     def requirements(self):
+        if self.settings.os == "Windows":
+            # conan-center
+            self.requires("7zip/19.00")
+        else:
+            # conan-center
+            self.requires("libsafec/3.6.0")
         if self.options.steam:
             self.requires("steamworks/1.5.1@storm/prebuilt")
 
@@ -31,9 +37,12 @@ class StormEngine(ConanFile):
 
     default_options = {
         "sdl2:sdl2main": False,
-        "sentry-native:backend": "crashpad",
-        "sentry-native:transport": "winhttp"
+        "sentry-native:backend": "crashpad"
     }
+
+    def configure(self):
+        if self.settings.os == "Windows":
+            self.default_options["sentry-native:transport"] = "winhttp"
 
     def imports(self):
         self.__dest = str(self.options.output_directory) + "/" + getenv("CONAN_IMPORT_PATH", "bin")
