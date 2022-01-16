4f41ef41fde7e9e65cdda476af9856e7bb177af8
Aleksey Komarov <q4arus@ya.ru>
2022-01-16 05:03:12 +0300

    [linux] replace LOWORD

--- src/libs/sea_cameras/src/free_camera.cpp
+++ src/libs/sea_cameras/src/free_camera.cpp
@@ -87,7 +87,7 @@ void FREE_CAMERA::Move(uint32_t DeltaTime)
 {
     if (!isActive())
         return;
-    if (LOWORD(GetKeyState(VK_NUMLOCK)) != 0)
+    if (GetKeyState(VK_NUMLOCK) & 1)
         return;
 
     // POINT pnt;
