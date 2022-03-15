353395e452e592a422bdecb559600a5dbfb695f6
Aleksey Komarov <q4arus@ya.ru>
2022-03-15 17:50:03 +0300

    [linux] first version for resource paths handling on linux Based on this file (GPLv3): https://github.com/KD-lab-Open-Source/Perimeter/blob/d131aa26dce1db187416718610baa09d53c65de8/Source/XTool/XUTIL/XUTIL.CPP

--- src/libs/renderer/src/s_device.cpp
+++ src/libs/renderer/src/s_device.cpp
@@ -1376,7 +1376,7 @@ int32_t DX9RENDER::TextureCreate(const char *fname)
         return -1L;
     }
 
-    std::filesystem::path path = convert_path_sep(fname);
+    std::filesystem::path path = fio->ConvertPathResource(fname);
     std::string pathStr = path.extension().string();
     if (storm::iEquals(pathStr, ".tx"))
         path.replace_extension();
