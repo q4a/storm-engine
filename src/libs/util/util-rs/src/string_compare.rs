c3e203d46a5412ca77cbad6803369361bfda058b
Vladimir Kuzmin <1559761+Asapin@users.noreply.github.com>
2022-04-14 21:07:50 +0900

    Simplified test data, oxidizedC++

--- src/libs/util/util-rs/src/string_compare.rs
+++ src/libs/util/util-rs/src/string_compare.rs
@@ -16,44 +16,52 @@ pub fn ignore_case_find(s: &str, pat: &str, start: usize) -> Option<usize> {
 #[cfg(test)]
 mod tests {
     use crate::string_compare::ignore_case_find;
-    use std::fs::File;
-    use std::io::{self, BufRead};
-    use std::path::Path;
 
     #[test]
-    fn index_of_test() {
-        let mut test_data = read_lines("control_data/ignore_case_index_of.txt").unwrap();
-        while let Some(s1) = test_data.next() {
-            let s1 = s1.unwrap();
-            let s2 = test_data.next().unwrap().unwrap();
-            let start = test_data
-                .next()
-                .unwrap()
-                .unwrap()
-                .parse::<usize>()
-                .expect("Not usize");
-            let expected_string = test_data.next().unwrap().unwrap();
-            let expected = match expected_string.parse::<i32>() {
-                Ok(v) => v,
-                Err(_) => panic!("<{}> is not i32", expected_string),
+    fn ignore_case_find_test() {
+        let dataset = vec![
+            ("GunPowder", "map_part", 0, -1),
+            ("Bridgetown_Lightman", "_Lightman", 0, 10),
+            ("banker_9_1", "banker", 0, 0),
+            (
+                "Sharle_1,Sharle_2,Sharle_3,Sharle_4,Sharle_5,protocusto,Sharle_6,Sharle_8",
+                ",",
+                0,
+                8,
+            ),
+            (
+                "Sharle_1,Sharle_2,Sharle_3,Sharle_4,Sharle_5,protocusto,Sharle_6,Sharle_8",
+                ",",
+                1,
+                8,
+            ),
+            (
+                "Sharle_1,Sharle_2,Sharle_3,Sharle_4,Sharle_5,protocusto,Sharle_6,Sharle_8",
+                ",",
+                9,
+                17,
+            ),
+            (
+                "Sharle_1,Sharle_2,Sharle_3,Sharle_4,Sharle_5,protocusto,Sharle_6,Sharle_8",
+                ",",
+                65,
+                -1,
+            ),
+            ("Ромп,Ронгар,Рорда,Рос,Россау,Роттир", ",", 0, 8),
+            ("Ромп,Ронгар,Рорда,Рос,Россау,Роттир", ",", 9, 21),
+            ("Ромп,Ронгар,Рорда,Рос,Россау,Роттир", ",", 53, -1),
+            ("Ромп,Ронгар,Рорда,Рос,Россау,Роттир", ",", 1, 8),
+        ];
+
+        for (s, pat, start, expected) in dataset {
+            let result = ignore_case_find(s, pat, start);
+            let expected = if expected == -1 {
+                None
+            } else {
+                Some(expected as usize)
             };
 
-            let result = ignore_case_find(&s1, &s2, start)
-                .map(|i| i as i32)
-                .unwrap_or(-1);
-            assert_eq!(
-                result, expected,
-                "s1: <{}>, s2: <{}>, start: <{}>",
-                &s1, &s2, start
-            );
+            assert_eq!(result, expected);
         }
-    }
-
-    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
     }
 }
