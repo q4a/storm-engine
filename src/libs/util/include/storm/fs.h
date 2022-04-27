cd1a330b577f3f95167169fccd09449b0016da1c
Aleksey Komarov <q4arus@ya.ru>
2022-04-27 11:40:27 +0300

    [linux] fix for mimalloc_fun for Windows and Linux
    Old bad behavior.
    In Windows and Linux mimalloc_fun will be called >= 2 times:
    1) Initialization will remove old file and will keep alive mimalloc_log_path until you exit.
    2) Not sure, but I did not see in Windows any other messages, but it's totally possible and I have at least one in Linux.
    3) When you press exit you: firstly go to the end of main "return EXIT_SUCCESS;" and after that calling mimalloc_fun
    
    In Windows after exiting main and running mimalloc_fun I got mimalloc_log_path.empty() == true: it remove all previous messages and write only last block of messages.
    In Linux after exiting main and running mimalloc_fun I got mimalloc_log_path.empty() == false, but it contain random non ascii chars =>
    it will not remove previous messages in mimalloc.log but will create random non ascii filename in current folder
    
    New behavior.
    1) For Windows and Linux mimalloc_fun will get "init" arg only once in main when calling mi_register_output and then it will remove old  mimalloc.log.
    2) All other calls will write to mimalloc.log without removing it.
    3) I have to remove static in GetStashPath() and from "static std::filesystem::path mimalloc_log_path;" to not get random non ascii in Linux.

--- src/libs/util/include/storm/fs.h
+++ src/libs/util/include/storm/fs.h
@@ -15,7 +15,7 @@ using namespace std::filesystem;
 
 inline path GetStashPath()
 {
-    static path path;
+    path path;
     if (path.empty())
     {
 #ifdef _WIN32 // FIX_LINUX SHGetKnownFolderPath
