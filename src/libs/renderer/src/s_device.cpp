cca027e8c1d06b813efd3bb7dda4816182751268
Aleksey Komarov <q4arus@ya.ru>
2022-05-22 23:15:04 +0300

    [linux] disable bSafeRendering in Linux

--- src/libs/renderer/src/s_device.cpp
+++ src/libs/renderer/src/s_device.cpp
@@ -1296,12 +1296,14 @@ bool DX9RENDER::DX9EndScene()
         LostRender();
     }
 
+#ifdef _WIN32 // bSafeRendering
     if (bSafeRendering)
     {
         const HDC dc = GetDC(hwnd);
         SetPixel(dc, 0, 0, 0);
         ReleaseDC(hwnd, dc);
     }
+#endif
 
     return true;
 }
