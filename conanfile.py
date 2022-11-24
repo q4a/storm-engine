2c109f2fc3726d279f5b61d45787aab9ca17f70a
Aleksey Komarov <q4arus@ya.ru>
2022-11-24 09:23:46 +0300

    conan: use find_package to add SDL dependency in Linux (without conan)

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class StormEngine(ConanFile):
 
     # dependencies used in deploy binaries
     # conan-center
-    requires = ["zlib/1.2.11", "spdlog/1.9.2", "fast_float/3.4.0", "sdl/2.0.18", "mimalloc/2.0.3", "sentry-native/0.5.0",
+    requires = ["zlib/1.2.11", "spdlog/1.9.2", "fast_float/3.4.0", "mimalloc/2.0.3", "sentry-native/0.5.0",
     # storm.jfrog.io
     "directx/9.0@storm/prebuilt", "fmod/2.02.05@storm/prebuilt"]
     # aux dependencies (e.g. for tests)
@@ -27,6 +27,7 @@ class StormEngine(ConanFile):
         if self.settings.os == "Windows":
             # conan-center
             self.requires("7zip/19.00")
+            self.requires("sdl/2.0.18")
         else:
             # conan-center
             self.requires("libsafec/3.6.0")
